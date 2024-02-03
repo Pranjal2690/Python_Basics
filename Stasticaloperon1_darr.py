97d32f4879c85b159ba37afb45519852f8b76dff
Pranjal Sinha <91946276+Pranjal2690@users.noreply.github.com>
2024-02-03 21:39:12 +0530

    Stastical operations on 1_Darray

---
+++ Stasticaloperon1_darr.py
@@ -0,0 +1,70 @@
+### Stastical Operation On One-Dimensional Array
+""" 
+ max()
+ min()
+ sum()
+ mean()
+ median()
+ prod()
+ variance()
+ stdeviation()
+
+ """
+
+# Max 
+
+"""import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.max(x)
+print(y)
+"""
+# Min
+
+"""import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.max(x)
+print(y)
+"""
+# Sum
+
+"""import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.sum(x)
+print(y)
+"""
+
+# Mean
+
+"""import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.mean(x)
+print(y)
+"""
+# Median
+
+"""import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.median(x)
+print(y)
+"""
+
+# Prod
+
+"""import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.prod(x)
+print(y)
+"""
+# Variance
+
+"""import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.var(x)
+print(y)"""
+
+# Standard deviation
+
+import numpy as np 
+x=np.array([6,5,4,3,2,1])
+y=np.std(x)
+print(y)
