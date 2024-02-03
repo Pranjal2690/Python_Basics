78b78f96bafd3cdb38df9ac49615ff69744d82cf
Pranjal Sinha <91946276+Pranjal2690@users.noreply.github.com>
2024-02-03 20:27:25 +0530

    arithematicoperationsonnumpyarray

---
+++ AritheOperatoronnp.py
@@ -0,0 +1,131 @@
+# Addition
+
+import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+
+"""# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3,4],[5,6,7]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4,5],[6,7,8]])
+print(y)
+z=x+y
+print(z)"""
+
+# Substraction 
+
+"""import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3,4],[5,6,7]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4,5],[6,7,8]])
+print(y)
+#z=x-y
+z=y-x
+print(z)"""
+
+#  Multiplication
+
+"""import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3,4],[5,6,7]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4,5],[6,7,8]])
+print(y)
+z=x*y
+print(z)"""
+
+# Matrix Multiplication
+
+"""import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3],[5,6]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4],[6,7]])
+print(y)
+z=x@y
+print(z)"""
+
+# Division
+
+"""import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3],[5,6]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4],[6,7]])
+print(y)
+# z=x/y
+z=y/x
+print(z)"""
+
+# Floor Division
+
+"""import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3],[5,6]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4],[6,7]])
+print(y)
+# z=x/y
+z=y//x
+print(z)"""
+
+# Exponentiation
+
+"""import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3],[5,6]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4],[6,7]])
+print(y)
+# z=x/y
+z=y**x
+print(z)"""
+
+# Modulus
+
+"""import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3],[5,6]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4],[6,7]])
+print(y)
+# z=x/y
+z=y%x
+print(z)"""
+
+# Transpose
+
+import numpy as np
+''' There has two way of taking given input one is direct other is giving range and values'''
+# x=np.arange(2,10).reshape(2,4) 
+x=np.array([[2,3],[5,6]])
+print(x)
+# y=np.arange(3,11).reshape(2,4)
+y=np.array([[3,4],[6,7]])
+print(y)
+
+# print(x.transpose())
+
+print(y.transpose())
+
+
+
+
+
+
+
