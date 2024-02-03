e7521c32edae54384e182e64cd7473733f07e8fd
Pranjal Sinha <91946276+Pranjal2690@users.noreply.github.com>
2024-02-04 00:01:30 +0530

    Added Some Read & Write Operations

--- File_Handling.py
+++ File_Handling.py
@@ -14,3 +14,19 @@ f.close
 
 f=open('Index.txt')
 print(f.read())
+
+f=open('Index.txt')
+print(f.read(5))
+print(f.read(5))
+print(f.tell())
+print(f.seek(0))
+print(f.seek(15))
+print(f.read(5))
+
+f.seek(0)
+for line in f:
+ print(line)
+
+
+f=open('Index.txt')
+f.readline()
