14cdd2aa79b2975e30f692a4f1c295ee9f236fed
Pranjal Sinha <91946276+Pranjal2690@users.noreply.github.com>
2024-02-04 00:32:40 +0530

    Scopes in Python

---
+++ Scopes_python.py
@@ -0,0 +1,19 @@
+glob="Hi I am Globally Availble"
+
+def f1():
+ """
+ Testing Scopes and Lifeline of a varaible
+ """
+ local="Hi I am locally avaiable"
+
+ print(local)
+ print(glob)
+
+f1()
+
+print(glob)
+
+ 
+
+
+
