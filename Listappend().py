b7940bde7d5faba6eea3d7d1307bce0643ef7085
Pranjal Sinha <91946276+Pranjal2690@users.noreply.github.com>
2024-02-04 18:22:13 +0530

    Append,Extend,add,update

---
+++ Listappend().py
@@ -0,0 +1,17 @@
+list=[1,2,3,4,5]
+## list.append([10,15])
+## print(list)
+
+list.extend([10,15])
+print(list)
+
+set_1={10,20,30}
+set_1.add(40)
+print(set_1)
+
+set_1.update({50})
+print(set_1)
+
+
+
+
